d77e6af712a91610bbad0d78353549716c2e5de4
koppalaaishwarya <aishwaryakoppala685@gmail.com>
2025-05-13 11:14:53 +0530

    modified changes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,13 +2,13 @@ import spark.servlet.SparkApplication;
 
 import static spark.Spark.get;
 
-public class HelloWorld implements SparkApplication {
+public class Helloworld implements SparkApplication {
 	public static void main(String[] args) {
-		new HelloWorld().init();
+		new Helloworld().init();
 	}
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World");
+		get("/hello", (req, res) -> "Hello world");
 	}
 }
